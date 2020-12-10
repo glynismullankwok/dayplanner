548902b66543ccac3948fba941ef841dbe774cec
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2020-12-10 16:58:41 -0500

    changed script.js

--- script.js
+++ script.js
@@ -1,108 +1,58 @@
-// var timeBlocks = $(".container");
+//array of workday hours
+var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
+var commentInput = document.querySelector("#comment.hours[i]")
+var userInputSpan = document.querySelector("#hours[i]")
 
 
-//var currentDay = new Date();
-
-// var timeDiv = $("#currentDay");
-// timeDiv.append(timeDisplay);
-
-// var saveButton = document.querySelectorAll("button");
-
-// var timeList = [
-//     "9am",
-//     "10am",
-//     "11am",
-//     "12pm",
-//     "1pm",
-//     "2pm",
-//     "3pm",
-//     "4pm",
-//     "5pm",
-// ];
-
-// var idTime = [
-//     "9",
-//     "10",
-//     "11",
-//     "12",
-//     "13",
-//     "14",
-//     "15",
-//     "16",
-//     "17"
-// ];
-
-// for (var i=0; i = timeList.length; i++){
-//     var newRow = $("<div class='row time-block'>").attr("id", idTime[i]);
-//     var newDiv2 = $("<div class='hour col-1'>");
-//     var newDiv = $("<textarea class='col-10'>");
-//     var newButt = $("<button type='button' class='saveBtn col-1 far fa-save'>");
-
-//     timeBlocks.append(newRow);
-
-//     newDiv2.text(timeList[i]);
-//     newRow.append(newDiv2);
-
-//     newDiv.text();
-//     newRow.append(newDiv);
-
-//     newButt.text();
-//     newRow.append(newButt);
+// var emailInput = document.querySelector("#email");
+// var passwordInput = document.querySelector("#password");
+// var signUpButton = document.querySelector("#sign-up");
+// var msgDiv = document.querySelector("#msg");
+// var userEmailSpan = document.querySelector("#user-email");
+// var userPasswordSpan = document.querySelector("#user-password");
 
 
-// } 
+// renderLastRegistered();
+function displayComment(type, comment){
+    for (i=0; i < hours.length; i++)
+    container.comment = comment.hours[i]
+}
+// LOCAL STORAGE
 
-// localStorageFunction();
-
-// function localStorageFunction(){
-//     $("textarea")[0].value = localStorage.getItem("textarea1");
-//     $("textarea")[1].value = localStorage.getItem("textarea2");
-//     $("textarea")[2].value = localStorage.getItem("textarea3");
-//     $("textarea")[3].value = localStorage.getItem("textarea4");
-//     $("textarea")[4].value = localStorage.getItem("textarea5");
-//     $("textarea")[5].value = localStorage.getItem("textarea6");
-//     $("textarea")[6].value = localStorage.getItem("textarea7");
-//     $("textarea")[7].value = localStorage.getItem("textarea8");
-//     $("textarea")[8].value = localStorage.getItem("textarea9");
+// function displayMessage(type, message) {
+//   msgDiv.textContent = message;
+//   msgDiv.setAttribute("class", type);
 // }
-// $("button").on("click", function(event) {
-//     event.preventDefault();
-//     var textArea1 = $("textarea")[0].value;
-//     var textArea2 = $("textarea")[1].value;
-//     var textArea3 = $("textarea")[2].value;
-//     var textArea4 = $("textarea")[3].value;
-//     var textArea5 = $("textarea")[4].value;
-//     var textArea6 = $("textarea")[5].value;
-//     var textArea7 = $("textarea")[6].value;
-//     var textArea8 = $("textarea")[7].value;
-//     var textArea9 = $("textarea")[8].value;
 
-//     localStorage.setItem("textarea1", textArea1);
-//     localStorage.setItem("textarea2", textArea2);
-//     localStorage.setItem("textarea3", textArea3);
-//     localStorage.setItem("textarea4", textArea4);
-//     localStorage.setItem("textarea5", textArea5);
-//     localStorage.setItem("textarea6", textArea6);
-//     localStorage.setItem("textarea7", textArea7);
-//     localStorage.setItem("textarea8", textArea8);
-//     localStorage.setItem("textarea9", textArea9);
+// function renderLastRegistered() {
+//   var email = localStorage.getItem("email");
+//   var password = localStorage.getItem("password");
+
+//   if (!email || !password) {
+//     return;
+//   }
+
+//   userEmailSpan.textContent = email;
+//   userPasswordSpan.textContent = password;
+// }
+
+// signUpButton.addEventListener("click", function(event) {
+//   event.preventDefault();
+
+//   var email = document.querySelector("#email").value;
+//   var password = document.querySelector("#password").value;
+
+//   if (email === "") {
+//     displayMessage("error", "Email cannot be blank");
+//   } else if (password === "") {
+//     displayMessage("error", "Password cannot be blank");
+//   } else {
+//     displayMessage("success", "Registered successfully");
+
+//     localStorage.setItem("email", email);
+//     localStorage.setItem("password", password);
+//     renderLastRegistered();
+//   }
 // });
 
-// function hourUpdater(){
-//     var currentHour = moment().hours();
-//     $(".time-block").each(function(){
-//         var blockHour = parseInt ($(this).attr("id").split(" ")[0]);
 
-//         if (blockHour < currentHour){
-//             $(this).addClass("past");
-//         }else if (blockHour === currentHour){
-//             $(this).removeClass("past");
-//             $(this).removeClass("present");
-//             $(this).addClass("future");
-//         }
-//     });
-// }
-
-// hourUpdater();
-
-// var checkTime = setInterval(hourUpdater, 15000)
