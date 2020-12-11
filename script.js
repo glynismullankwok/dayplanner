69d8e0600667414a8d5d8388f4190b5f1b0c62f4
Glynis Mullan-Kwok <glynismullankwok@gmail.com>
2020-12-10 19:22:07 -0500

    script.js updated

--- script.js
+++ script.js
@@ -4,13 +4,7 @@ var hours = ["9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm"];
 // var userInputSpan = document.querySelector("#hours[i]")
 
 displayComment()
-// var emailInput = document.querySelector("#email");
-// var passwordInput = document.querySelector("#password");
-// var signUpButton = document.querySelector("#sign-up");
-// var msgDiv = document.querySelector("#msg");
-// var userEmailSpan = document.querySelector("#user-email");
-// var userPasswordSpan = document.querySelector("#user-password");
-
+//buttons click to save text in comment box
 $(".saveBtn").on("click",function(){
     console.log(this)
     //console.log(
@@ -22,7 +16,7 @@ $(".saveBtn").on("click",function(){
     localStorage.setItem(lsKey, lsValue)
 })
 
-// renderLastRegistered();
+// display comment
 function displayComment(type, comment){
     for (var i=0; i < hours.length; i++){
         console.log(hours[i])
@@ -35,47 +29,8 @@ function displayComment(type, comment){
             $(customId)
         )
     }
-    //container.comment = comment.hours[i]
+   
 }
-// LOCAL STORAGE
 
-// function displayMessage(type, message) {
-//   msgDiv.textContent = message;
-//   msgDiv.setAttribute("class", type);
-// }
-
-// function renderLastRegistered() {
-//   var email = localStorage.getItem("email");
-//   var password = localStorage.getItem("password");
-
-//   if (!email || !password) {
-//     return;
-//   }
-
-//   userEmailSpan.textContent = email;
-//   userPasswordSpan.textContent = password;
-// }
-//textButton.addEventListener("click", function(event){
-    //event.preventDefault();
-//})
-
-// signUpButton.addEventListener("click", function(event) {
-//   event.preventDefault();
-
-//   var email = document.querySelector("#email").value;
-//   var password = document.querySelector("#password").value;
-
-//   if (email === "") {
-//     displayMessage("error", "Email cannot be blank");
-//   } else if (password === "") {
-//     displayMessage("error", "Password cannot be blank");
-//   } else {
-//     displayMessage("success", "Registered successfully");
-
-//     localStorage.setItem("email", email);
-//     localStorage.setItem("password", password);
-//     renderLastRegistered();
-//   }
-// });
 
 
